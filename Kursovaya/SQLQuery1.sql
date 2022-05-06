191a1f27686cb9b99e2cd41ec72c0293eca2449e
Howe251 <misha.sosnin245@gmail.com>
2022-05-06 18:57:52 +0300

    Добавлен AUTOINCREMENT для полей id

--- Kursovaya/SQLQuery1.sql
+++ Kursovaya/SQLQuery1.sql
@@ -1,49 +1,49 @@
 USE SQLKursach;
 
---DROP TABLE conditer_store, conditer_suplier, conditer_suplied, dbo.conditer_ingredients, dbo.conditer_menu, 
+--DROP TABLE conditer_store, conditer_suplier, conditer_produced, dbo.conditer_ingredients, dbo.conditer_menu, 
 --dbo.conditer_menu_ingredients, conditer_menu_transfer_to;
 
 CREATE TABLE conditer_ingredients (
-  id bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
   [name] varchar(255) NOT NULL,
   [count] int NOT NULL,
   [postavka_id] bigint NOT NULL 
 );
 
 CREATE TABLE conditer_menu (
-  id bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
   [name] varchar(255) NOT NULL,
   add_date datetime2 NOT NULL DEFAULT(getdate()),
   cost int NOT NULL
 );
 
 CREATE TABLE conditer_menu_ingredients (
-  id bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
   menu_id bigint NOT NULL,
   ingredients_id bigint NOT NULL
 );
 
 CREATE TABLE conditer_menu_transfer_to (
-  id bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
   menu_id bigint NOT NULL,
   store_id bigint NOT NULL
 );
 
 CREATE TABLE conditer_store (
-  id bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
   store_name varchar(255) NOT NULL,
   [address] varchar(255) NOT NULL
 );
 
 CREATE TABLE conditer_suplier (
-  id bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
   company_name varchar(255) NOT NULL,
   item varchar(255) NOT NULL
 );
 
 CREATE TABLE conditer_produced (
-  id bigint NOT NULL PRIMARY KEY,
-  [produce_date] datetime2 NOT NULL DEFAULT(getdate()),
+  id bigint NOT NULL PRIMARY KEY IDENTITY(1,1),
+  [produce_date] date NOT NULL DEFAULT(getdate()),
   [count] bigint NOT NULL,
   menu_id bigint NOT NULL
 );
