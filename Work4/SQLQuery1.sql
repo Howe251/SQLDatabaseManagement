d3bc9b00a226b3e5effec7acbfcd8a31426b1e23
Howe251 <misha.sosnin245@gmail.com>
2022-05-10 18:44:05 +0300

    Лаба4 SQLQuery1

---
+++ Work4/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿--Показать сотрудника, который работает дольше всех
+SELECT p.FirstName, p.MiddleName, p.LastName, DATEDIFF(YEAR,e.HireDate,GETDATE()) AS WorkingAge 
+FROM Person.Person AS p
+INNER JOIN HumanResources.Employee as e
+ON p.BusinessEntityID = e.BusinessEntityID
+WHERE e.HireDate = (
+	SELECT MIN(HireDate)
+	FROM HumanResources.Employee
+);
